122c7993e6ce971147d139f5313f9f3743697429
Dipper <1023128147@qq.com>
2022-10-26 21:17:02 +0800

    创建Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money" />
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="labels" />
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics" />
+      统计
+    </router-link>
   </div>
 </template>
 
