e892605537dfc05f0624d7d4c9dfb3bbb3bfd4ea
Dipper <1023128147@qq.com>
2022-10-26 21:36:47 +0800

    添加active-class

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money" />
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels" />
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics" />
       统计
     </router-link>
@@ -36,6 +36,9 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+  > .item.selected {
+    color: red;
   }
 }
 </style>
