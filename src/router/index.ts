53b853464b3ce583ee0c583607899d6f354f0d3b
Dipper <1023128147@qq.com>
2022-10-26 17:09:49 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -23,6 +23,10 @@ const routes: Array<RouteConfig> = [
   {
     path: "/statistics",
     component: Statistics,
+  },
+  {
+    path: "*",
+    component: NotFound,
   },
 ];
 
