75f32d98c42dccab547f8bd7b36740046ec2a5e6
Dipper <1023128147@qq.com>
2022-10-26 16:29:16 +0800

    创建三个页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Labels.vue</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Statistics.vue</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
