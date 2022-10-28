60cff91ef2eba7da64442f40993f493e3fa9ff1f
Dipper <1023128147@qq.com>
2022-10-28 19:07:42 +0800

    HTML和CSS

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
       <slot></slot>
     </div>
     <Nav />
   </div>
 </template>
 
+<script lang="ts">
+export default {
+  props: ["classPrefix"],
+  name: "Layout",
+};
+</script>
+
 <style lang="scss" scoped>
-.nav-wrapper {
+.layout-wrapper {
   display: flex;
   flex-direction: column;
   height: 100vh;
