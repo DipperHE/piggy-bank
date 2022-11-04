ddbe916a30f00c94d0750b603d4bf8f1ea6bf78b
Dipper <1023128147@qq.com>
2022-11-05 00:44:04 +0800

    可以选中或取消 tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,11 +3,11 @@
     <NumberPad />
     <Types />
     <Notes />
-    <Tags />
+    <Tags :dataSource="tags" />
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
@@ -15,6 +15,11 @@ import Tags from "@/components/Money/Tags.vue";
 export default {
   name: "Money",
   components: { Tags, Notes, Types, NumberPad },
+  data() {
+    return {
+      tags: ["衣", "食", "住", "行"],
+    };
+  },
 };
 </script>
 
