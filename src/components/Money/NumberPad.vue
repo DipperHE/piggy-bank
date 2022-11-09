120a8f44c267fb85005c27ae530d79bc7668f7eb
Dipper <1023128147@qq.com>
2022-11-09 21:12:04 +0800

    添加 createdAt 字段

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -58,6 +58,7 @@ export default class NumberPad extends Vue {
   ok() {
     this.$emit("update:value", this.output);
     this.$emit("submit", this.output);
+    this.output = "0";
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{ record }}
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
@@ -15,18 +14,20 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
+const records = JSON.parse(window.localStorage.getItem("records") || "[]");
 type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
+  createAt?: Date;
 };
 @Component({
   components: { Tags, Notes, Types, NumberPad },
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
-  records: Record[] = [];
+  records: Record[] = records;
   record: Record = { tags: [], notes: "", type: "-", amount: 0 };
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -38,7 +39,8 @@ export default class Money extends Vue {
     this.record.amount = parseFloat(value);
   }
   saveRecord() {
-    const record2 = JSON.parse(JSON.stringify(this.record));
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createAt = new Date();
     this.records.push(record2);
   }
   @Watch("records")
