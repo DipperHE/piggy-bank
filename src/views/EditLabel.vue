f3d9d7193c2d141db2f744efbc12d1c47d440513
Dipper <1023128147@qq.com>
2022-11-12 00:24:46 +0800

    新增 EditLabel 组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -3,10 +3,24 @@
 </template>
 
 <script lang="ts">
+import tagsModel from "@/models/tagsModel";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+
 @Component
-export default class EditLabel extends Vue {}
+export default class EditLabel extends Vue {
+  created() {
+    const id = this.$route.params.id;
+    tagsModel.fetch();
+    const tags = tagsModel.data;
+    const tag = tags.filter((t) => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
