9f82aafd915973c595115b9917d4135e17521324
Dipper <1023128147@qq.com>
2022-11-12 23:05:18 +0800

    value 从 data 变成了 prop

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -12,7 +12,7 @@ import Vue from "vue";
 import { Component, Prop, Watch } from "vue-property-decorator";
 @Component
 export default class FormItem extends Vue {
-  value = "";
+  @Prop({ default: "" }) value!: string;
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
   @Watch("value")
