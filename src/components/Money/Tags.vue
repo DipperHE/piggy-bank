25988045ee0c775d3adb067044142e356eb941e0
Dipper <1023128147@qq.com>
2022-11-12 23:04:19 +0800

    消除 Tags 的警告，目前还存在 bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -6,11 +6,11 @@
     <ul class="current">
       <li
         v-for="tag in dataSource"
-        :key="tag"
+        :key="tag.id"
         :class="{ selected: selectedTags.indexOf(tag) >= 0 }"
         @click="toggle(tag)"
       >
-        {{ tag }}
+        {{ tag.name }}
       </li>
     </ul>
   </div>
