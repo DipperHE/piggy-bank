eca6657e088a20a8fea7b5901825896fdaab27f8
Dipper <1023128147@qq.com>
2022-11-13 22:40:09 +0800

    模块化的 store，删除了 models

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,6 +1,29 @@
-import recordsModel from "@/models/recordsModel";
+import clone from "@/lib/clone";
 
-export default {
-  recordList: recordsModel.fetch(),
-  createRecord: (record: RecordItem) => recordsModel.create(record),
+const localStorageKeyName = "recordList";
+
+const recordStore = {
+  recordList: [] as RecordItem[],
+  fetchRecords() {
+    this.recordList = JSON.parse(
+      window.localStorage.getItem(localStorageKeyName) || "[]"
+    ) as RecordItem[];
+    return this.recordList;
+  },
+  saveRecords() {
+    window.localStorage.setItem(
+      localStorageKeyName,
+      JSON.stringify(this.recordList)
+    );
+  },
+  createRecord(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.recordList?.push(record2); // 可选链语法
+    recordStore.saveRecords();
+  },
 };
+
+recordStore.fetchRecords();
+
+export default recordStore;
