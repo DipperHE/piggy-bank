b0c4df7422c8d8a99baf9bf868c6ec2bffe9280e
Dipper <1023128147@qq.com>
2022-11-13 14:52:18 +0800

    重新封装 recordListModel

--- src/models/recordsModel.ts
+++ src/models/recordsModel.ts
@@ -1,15 +1,17 @@
 const localStorageKeyName = "records";
 const recordsModel = {
+  data: [] as RecordItem[],
   clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
   fetch() {
-    return JSON.parse(
+    this.data = JSON.parse(
       window.localStorage.getItem(localStorageKeyName) || "[]"
     ) as RecordItem[];
+    return this.data;
   },
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data)));
   },
 };
 
