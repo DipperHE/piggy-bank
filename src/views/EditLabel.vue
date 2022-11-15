414d9117637e9acef2d5784adeba8fe35501f5f6
Dipper <1023128147@qq.com>
2022-11-15 21:03:44 +0800

    成功全部使用 Vuex 来进行数据管理

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="form-wrapper">
       <FormItem
-        :value="tag.name"
+        :value="currentTag.name"
         @update:value="update"
         fieldName="标签名"
         placeholder="请输入标签名"
@@ -29,28 +29,28 @@ import { Component } from "vue-property-decorator";
   components: { FormItem, Button },
 })
 export default class EditLabel extends Vue {
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag;
   }
   created() {
     const id = this.$route.params.id;
     this.$store.commit("fetchTags");
     this.$store.commit("setCurrentTag", id);
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace("/404");
     }
   }
   update(name: string) {
-    if (this.tag) {
+    if (this.currentTag) {
       this.$store.commit("updateTag", {
-        id: this.tag.id,
+        id: this.currentTag.id,
         name,
       });
     }
   }
   remove() {
-    if (this.tag) {
-      this.$store.commit("removeTag", this.tag.id);
+    if (this.currentTag) {
+      this.$store.commit("removeTag", this.currentTag.id);
     }
   }
   goBack() {
