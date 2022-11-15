382c63be91cab5d82feb6f267e7bf735ab150f78
Dipper <1023128147@qq.com>
2022-11-15 20:11:57 +0800

    Labels.vue 使用 Vuex，抽离 TagHelper 作为 mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li
@@ -19,6 +19,9 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
+import { mixins } from "vue-class-component";
+import TagHelper from "@/mixins/TagHelper";
+
 @Component({
   computed: {
     tagList() {
@@ -26,7 +29,7 @@ import { Component, Prop } from "vue-property-decorator";
     },
   },
 })
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
   created() {
     this.$store.commit("fetchTags");
@@ -39,13 +42,6 @@ export default class Tags extends Vue {
       this.selectedTags.push(tag);
     }
     this.$emit("update:value", this.selectedTags);
-  }
-  create() {
-    const name = window.prompt("请输入标签名");
-    if (!name) {
-      return window.alert("标签名不能为空");
-    }
-    this.$store.commit("createTag", name);
   }
 }
 </script>
@@ -89,3 +85,6 @@ export default class Tags extends Vue {
   }
 }
 </style>
+
+function TagHelper(TagHelper: any) { throw new Error("Function not
+implemented."); }
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -21,19 +21,20 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Button from "@/components/Button.vue";
+import { mixins } from "vue-class-component";
+import TagHelper from "@/mixins/TagHelper";
 
 @Component({
   components: { Button },
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
+    },
+  },
 })
-export default class Labels extends Vue {
-  // TODO
-  tags = []; // store.tagList;
-  createTag() {
-    const name = window.prompt("请输入标签名");
-    if (name) {
-      // TODO
-      // store.createTag(name);
-    }
+export default class Labels extends mixins(TagHelper) {
+  beforeCreate() {
+    this.$store.commit("fetchTags");
   }
 }
 </script>
