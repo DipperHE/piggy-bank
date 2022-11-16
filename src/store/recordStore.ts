fd0a96a0ddf2b9ae54a3a956b1d1061a48362c8a
Dipper <1023128147@qq.com>
2022-11-16 22:34:47 +0800

    完成列表的 CSS

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -18,7 +18,7 @@ const recordStore = {
   },
   createRecord(record: RecordItem) {
     const record2: RecordItem = clone(record);
-    record2.createdAt = new Date();
+    record2.createdAt = new Date().toISOString();
     this.recordList?.push(record2); // 可选链语法
     recordStore.saveRecords();
   },
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -6,6 +6,18 @@
       :dataSource="intervalList"
       :value.sync="interval"
     />
+    <ol>
+      <li v-for="(group, index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }} </span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -20,12 +32,54 @@ import recordTypeList from "@/constants/recordTypeList";
   components: { Tabs },
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? "无" : tags.join(",");
+  }
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+  get result() {
+    const { recordList } = this;
+    type HashTableValue = { title: string; items: RecordItem[] };
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split("T");
+      hashTable[date] = hashTable[date] || { title: date, items: [] };
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+  beforeCreate() {
+    this.$store.commit("fetchRecords");
+  }
   type = "-";
   interval = "day";
   intervalList = intervalList;
   recordTypeList = recordTypeList;
 }
 </script>
+
+<style lang="scss" scoped>
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title {
+  @extend %item;
+}
+.record {
+  background: white;
+  @extend %item;
+}
+.notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
+</style>
 
 <style lang="scss" scoped>
 ::v-deep {
