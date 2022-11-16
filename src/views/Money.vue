6e5f07182c7b28d8de73f2759d317291ba97faf6
Dipper <1023128147@qq.com>
2022-11-16 21:16:16 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,7 +16,6 @@
 <script lang="ts">
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component } from "vue-property-decorator";
@@ -24,7 +23,7 @@ import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList";
 
 @Component({
-  components: { Tags, FormItem, Types, NumberPad, Tabs },
+  components: { Tags, FormItem, NumberPad, Tabs },
 })
 export default class Money extends Vue {
   get recordList() {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -10,7 +10,6 @@
 </template>
 
 <script lang="ts">
-import Types from "@/components/Money/Types.vue";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
@@ -18,7 +17,7 @@ import intervalList from "@/constants/intervalList";
 import recordTypeList from "@/constants/recordTypeList";
 
 @Component({
-  components: { Types, Tabs },
+  components: { Tabs },
 })
 export default class Statistics extends Vue {
   type = "-";
