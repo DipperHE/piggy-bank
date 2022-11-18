0c298e0ebf8a11bf4618296ce0d0950372928fde
Dipper <1023128147@qq.com>
2022-11-18 23:42:26 +0800

    部署网站到 piggy-bank-website

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/piggy-bank-website/" : "/",
   lintOnSave: false,
   chainWebpack: (config) => {
     const dir = path.resolve(__dirname, "src/assets/icons");
